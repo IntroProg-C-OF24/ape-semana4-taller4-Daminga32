9b4b152c79da6c09085c02af71e69596525691da
Dario <daminga2@utpl.edu.ec>
2023-11-01 22:28:41 -0500

    Ya

--- APE-Semana4/src/ape/semana4/TablaMultiplicar_3.java
+++ APE-Semana4/src/ape/semana4/TablaMultiplicar_3.java
@@ -1,6 +1,39 @@
 package ape.semana4;
+
+import java.util.Scanner;
+
 public class TablaMultiplicar_3 {
+
     public static void main(String[] args) {
-        
+        int num;
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int num5;
+        int num6;
+        int num7;
+        int num8;
+        int num9;
+        int num10;
+        int num11;
+        int num12;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese un numero que se encuntre entre los numeros 2 y 6: ");
+        num = teclado.nextInt();
+        num1 = (num * 1);
+        num2 = (num * 2);
+        num3 = (num * 3);
+        num4 = (num * 4);
+        num5 = (num * 5);
+        num6 = (num * 6);
+        num7 = (num * 7);
+        num8 = (num * 8);
+        num9 = (num * 1);
+        num10 = (num * 10);
+        num11 = (num * 11);
+        num12 = (num * 12);
+        System.out.println("La tabla del numero leido es: " + num1  + num2  + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10 + num11 + num12);
     }
+
 }
